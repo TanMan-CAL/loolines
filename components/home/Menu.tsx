78503a5dce79b7ce4c1cdacb77ebda1c2c5534d9
adveyn <adveynandan@gmail.com>
2024-11-27 00:11:42 -0500

    full model code

--- components/home/Menu.tsx
+++ components/home/Menu.tsx
@@ -4,6 +4,7 @@ import { createSupabaseClient } from "@/lib/supabase/client";
 import React, { useEffect, useState } from "react";
 import RestaurantChip from "../ui/RestaurantChip";
 import { useRestaurant } from "@/lib/supabase/useRestaurant";
+import PredictionChip from "../ui/PredictionChip";
 const supabase = createSupabaseClient();
 interface CustomerData {
   restaurants: string;
@@ -59,6 +60,7 @@ export default function Menu() {
     return () => clearInterval(interval);
   }, []);
   return (
+    <div>
     <div className="absolute top-4 left-4 z-50 flex flex-col gap-2 p-6 pt-4  border w-fit rounded-lg bg-[#1b1c25] border-[#151515]">
       <div className="flex place-items-baseline gap-3">
         <h1 className="font-mono text-[60px] text-white">{time}</h1>
@@ -71,6 +73,10 @@ export default function Menu() {
         times={"10:00 - 19:30"}
         count={count}
       />
+    </div>
+    <div className="absolute top-4 right-4 z-50 flex flex-col gap-2 p-6 pt-4 border w-fit rounded-lg">
+        <PredictionChip />
+      </div>
     </div>
   );
 }
