ac239de22568f8d59be2852cc838164f86ebfbfe
Tanmay Shah <tanmayshah2007@gmail.com>
2025-01-22 01:11:31 -0500

    forked repo

--- constants/restaurants.ts
+++
@@ -1,9 +0,0 @@
-export const restaurants_slc = [
-  { name: "Subway", hours: "10:00 am - 7:30 pm" },
-  { name: "Shawarma Hub", hours: "10:00 am - 7:30 pm" },
-  { name: "Quesada", hours: "10:00 am - 7:30 pm" },
-  { name: "Teriyaki Experience", hours: "10:00 am - 7:30 pm" },
-  { name: "Meat Place", hours: "10:00 am - 7:30 pm" },
-  { name: "Pizza Pizza / Masala", hours: "10:00 am - 7:30 pm" },
-  { name: "Indian Street Food", hours: "10:00 am - 7:30 pm" },
-];
